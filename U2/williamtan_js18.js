79711038248ac7a280d1d186cb751c8ea5f7d0fe
WilliamTan2704 <william.h.tan27@gmail.com>
2022-09-22 10:30:43 +0700

    fix random rgb to include 255

--- U2/williamtan_js18.js
+++ U2/williamtan_js18.js
@@ -1,8 +1,8 @@
 function generateText() {
     let text = document.getElementById("text");
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     let div = document.getElementById("div");
     let h1 = document.createElement("h1");
     h1.style.color = `rgb(${r}, ${g}, ${b})`
